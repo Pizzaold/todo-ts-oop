9cbe09ec806d2857c54ef4134c57db87bbbb674d
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-01-23 10:06:58 +0200

    Create new Todo from user data

---
+++ src/controllers/todos.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { Todo } from "../models/todo";
+
+const todos: Todo[] = [];
+
+export const createTodo = (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const task = (req.body as {task: string}).task;
+        const newTodo = new Todo (Math.random().toString(), task);
+        todos.push(newTodo);
+        res.status(201).json({
+            message: 'Created new todo.',
+             createdTodo: newTodo
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+};
