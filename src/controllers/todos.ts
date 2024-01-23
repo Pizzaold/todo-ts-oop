5568dbf7d32266eb94b40284543bfe23ccf5fde8
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-01-23 10:21:40 +0200

    Update the Todo by id

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -25,3 +25,18 @@ export const getTodos = (req: Request, res: Response, next: NextFunction) => {
         console.log(err);
     }
 };
+
+export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const todoId = req.params.id;
+        const updatedTask = (req.body as {task: string}).task;
+        const todoIndex = todos.findIndex(todo => todo.id === todoId);
+        if(todoIndex < 0){
+            throw new Error('Could not find todo.');
+        }
+        todos[todoIndex] = new Todo (todos[todoIndex].id, updatedTask);
+        res.status(201).json({message: 'Updated todo.', updatedTodo: todos[todoIndex]});
+    } catch(err){
+        console.log(err);
+    }
+};
